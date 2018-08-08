f3f51caf050eb078a48cb06a804a30cf06bdf673
Reija Jokinen <reija.jokinen@kolumbus.fi>
2018-08-08 16:15:50 +0300

    welcome

--- src/main/resources/static/js/features.js
+++ src/main/resources/static/js/features.js
@@ -2,6 +2,8 @@ function login() {
     document.getElementById("login").onclick = function() {
         console.log("Klikattu regs");
         window.location.assign("http://localhost:8080/topics.html");
+        document.getElementById("welcome").removeClass("hide");
+        welcome(username);
     }
 }
 
@@ -17,4 +19,8 @@ function continueWithoutRegistering() {
         console.log("Klikattu regs");
         window.location.assign("http://localhost:8080/topics.html");
     }
+}
+
+function welcome(username) {
+    $("<h3></h3>").text("Tervetuloa " + username + "!").appendTo("#welcome");
 }
